45736c9167c34776ab957ea91653f69980a36227
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-22 17:37:04 +0300

    3. вопрос-ответ

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
-let question = prompt('Сколько фильмов вы уже посмотрели?', '');
-let numberOfFilms = +question;
-console.log(numberOfFilms);
-
+let  numberOfFilms  = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let film = prompt('Один из последних просмотренных фильмов?', '');
+let grade = +prompt('На сколько оцените его?');
+let film2 = prompt('Один из последних просмотренных фильмов?', '');
+let grade2 = +prompt('На сколько оцените его?');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,3 +11,8 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+personalMovieDB.movies[film] = grade;
+personalMovieDB.movies[film2] = grade2;
+
+
+console.log(personalMovieDB);
