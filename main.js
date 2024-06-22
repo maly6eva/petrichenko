fd66bb094850705ee8e162a08d476cf73fbecc97
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-22 17:00:11 +0300

    2. Создаем объект personalMovieDB

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 let question = prompt('Сколько фильмов вы уже посмотрели?', '');
 let numberOfFilms = +question;
 console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
