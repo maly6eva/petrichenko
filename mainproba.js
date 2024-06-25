1a8a3f662d242f9908669eeae9a859a710cd9f30
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-25 18:07:51 +0300

    каламбур

--- mainproba.js
+++ mainproba.js
@@ -1,14 +1,35 @@
-//задачка звездочка
-
-let result = '';
-const length = 7;
-
-for (let i = 1; i < length; i++) {
-    for (let j = 0; j < i; j++) {
-        result = result + '*';
-
+// пример
+ for(let i = 0; i< 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j)
     }
-    result = result + '\n'
-
 }
-console.log(result)
+
+
+//1задачка звездочка
+
+// let result = '';
+// const length = 7;
+//
+// for (let i = 1; i < length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result = result + '*';
+//
+//     }
+//     result = result + '\n'
+//
+// }
+// console.log(result)
+
+//каламбур
+// first: for(let i = 0; i < 5; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 5; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if (k === 3) break first
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
