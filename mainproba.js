107d049295b25d451e43eef30107728e34be54e1
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-25 08:39:18 +0300

    звездочка доработка

--- mainproba.js
+++ mainproba.js
@@ -1,14 +1,14 @@
-let result = '*';
+//задачка звездочка
+
+let result = '';
 const length = 7;
-console.log(result)
 
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result = result + '*';
 
-for(let i = 0; i < 7; i++){
-    result = result + result;
-    console.log(result)
-    for(let j = 0; j <  length; j++){
-        result = result + result
-        console.log(result)
     }
+    result = result + '\n'
 
 }
+console.log(result)
